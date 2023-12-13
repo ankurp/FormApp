fbc35b193f7185e384f8f808732aa037ec8554bd
Ankur Patel <ankur@encoredevlabs.com>
2023-12-12 23:34:40 -0800

    Updating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
+form = Form.create(name: "Sign up User")
+form.form_attributes << FormAttribute.create(label: "Name", field_type: "text_field", is_required: true)
+form.form_attributes << FormAttribute.create(label: "Date of Birth", field_type: "date_field", is_required: true)
+form.form_attributes << FormAttribute.create(label: "Password", field_type: "password_field", is_required: true)
 user = User.create(name: "DHH")
-attribute = FormAttribute.create(label: "Name", field_type: "input", is_required: true)
-form = Form.create(name: "User Form")
-form.form_attributes << attribute
 user.forms << form
